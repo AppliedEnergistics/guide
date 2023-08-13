b3b2ee1952e4e23b5657046e89ce471fe4827ee1
Sebastian Hartte <shartte@users.noreply.github.com>
2023-08-13 18:56:52 +0200

    Improved error reporting when loading guide versions fails.

--- src/GuideVersionIndexLoader.tsx
+++ src/GuideVersionIndexLoader.tsx
@@ -6,11 +6,34 @@ import {
 } from "./data/GuideVersionIndex.ts";
 import Loading from "./components/Loading.tsx";
 import useLoadEffect from "./data/useLoadEffect.ts";
+import LoadError from "./components/LoadError.tsx";
+
+function resolveRelativeUrl(requestedUrl: string, relativeUrl: string) {
+  return new URL(relativeUrl, requestedUrl).toString();
+}
+
+// Allow relative or absolute URLs in the index, but resolve them to absolute URLs while loading
+async function transformIndex(response: Response): Promise<GuideVersionIndex> {
+  const root: GuideVersionIndex = await response.json();
+
+  if (!Array.isArray(root.versions)) {
+    throw new Error("Version index is corrupted");
+  }
+
+  return {
+    ...root,
+    versions: root.versions.map((version) => ({
+      ...version,
+      url: resolveRelativeUrl(response.url, version.url),
+    })),
+  };
+}
 
 function GuideVersionIndexLoader({ children }: PropsWithChildren) {
   const loadingResult = useLoadEffect<GuideVersionIndex>(
     guideAssetsUrl,
-    "guide versions"
+    "guide versions",
+    transformIndex
   );
 
   if (loadingResult.state === "success") {
@@ -21,12 +44,7 @@ function GuideVersionIndexLoader({ children }: PropsWithChildren) {
     );
   } else if (loadingResult.state === "error") {
     return (
-      <div>
-        ERROR
-        <button type="button" onClick={() => loadingResult.retry()}>
-          Retry
-        </button>
-      </div>
+      <LoadError operation={"Loading guide versions"} result={loadingResult} />
     );
   } else {
     return <Loading />;
